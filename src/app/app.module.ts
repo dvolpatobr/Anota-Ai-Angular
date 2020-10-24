664c2431ccbb3be932da413cc49d0a82eecc3629
Ranieri <57837805+xRanierix@users.noreply.github.com>
2020-10-24 18:47:17 -0300

    2 componentes, bootstrap e angular material instalado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UsuarioCadastroComponent } from './usuario-cadastro/usuario-cadastro.component';
+import { LembreteComponent } from './lembrete/lembrete.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsuarioCadastroComponent,
+    LembreteComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
