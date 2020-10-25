b680a975e0cf6ae199ba49219a81bfb0c3e4cb0e
Ranieri <57837805+xRanierix@users.noreply.github.com>
2020-10-25 13:38:50 -0300

    Construindo caixa de Meus Lembretes no lembrete.component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {MatGridListModule} from '@angular/material/grid-list';
 import { HomeComponent } from './home/home.component';
 import {MatTabsModule} from '@angular/material/tabs';
 
+
 @NgModule({
   declarations: [
     AppComponent,
